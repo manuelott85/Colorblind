4c0b291a69138ff765566b0493a673defbf0474d
Manuel Ott <dasmaeh85@gmx.at>
2018-01-27 12:51:33 +0100

    ColorCalibration WIP

---
+++ Assets/Scripts/colorCalibration/colorCalibration.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class colorCalibration : MonoBehaviour {
+
+    Material mat;
+    public bool isItGreen = true;
+
+    // Use this for initialization
+    void Start () {
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(mat.GetFloat("_HueShift"));
+    }
+
+    public void setSaturation(Slider slider)
+    {
+        mat.SetFloat("_Sat", slider.value);
+        if(isItGreen)
+            GameManager.instance.green_saturation = slider.value;
+        else
+            GameManager.instance.red_saturation = slider.value;
+    }
+    public void setValue(Slider slider)
+    {
+        mat.SetFloat("_Val", slider.value);
+        if (isItGreen)
+            GameManager.instance.green_saturation = slider.value;
+        else
+            GameManager.instance.red_saturation = slider.value;
+    }
+    public void setHueShift(Slider slider)
+    {
+        mat.SetFloat("_HueShift", slider.value);
+        if (isItGreen)
+            GameManager.instance.green_saturation = slider.value;
+        else
+            GameManager.instance.red_saturation = slider.value;
+    }
+}
