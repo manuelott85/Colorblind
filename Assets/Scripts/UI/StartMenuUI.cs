4e42164b5d378f61414aab1e1520f7abdc266ef4
Rüdiger Brandis <post@rbrandis.eu>
2018-04-15 22:09:51 +0200

    - Added StartMenu with ExitButton already functioning (I hope so, didn't test it yet) - The StartButton doesn't do anything yet - Set up StartMenuUI script for all the menu functions, Application.Quit() is already in it

---
+++ Assets/Scripts/UI/StartMenuUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuUI : MonoBehaviour {
+
+    public void ExitGame()
+    {
+        Debug.Log("QUIT GAME!");
+        Application.Quit();
+    }
+}
