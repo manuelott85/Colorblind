fdca6a2b73dcc20673cba0b303b37a6e35e19c3e
Manuel Ott <manuel.ott@gmx.at>
2018-09-12 13:55:26 +0200

    Player does move with the platforms

---
+++ Assets/Scripts/movement/moveWithPlatform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveWithPlatform : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject);
+        if(collision.gameObject == GameManager.instance.playerA.gameObject || collision.gameObject == GameManager.instance.playerB.gameObject)
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == GameManager.instance.playerA.gameObject || collision.gameObject == GameManager.instance.playerB.gameObject)
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
